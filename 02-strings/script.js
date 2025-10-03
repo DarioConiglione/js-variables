3c2d741ed6103f0daa335ac1b04412e22b000406
DarioConiglione <dariocng13@gmail.com>
2025-10-03 14:41:13 +0100

    ex. 2 of strings done

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -8,6 +8,15 @@ console.log(name);
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+const number = 8;
+console.log(number);
+
+const text = "8"
+console.log(text);
+
+const bool = true;
+console.log(bool);
+
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
